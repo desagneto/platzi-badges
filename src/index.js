55d93a3ced0ff5a281218c2b030d421b2c60139a
Andres Aguilar Nieto <desagneto@gmail.com>
2020-01-06 14:22:49 -0500

    clase nuestra primera pagina

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import "bootstrap/dist/css/bootstrap.css"
 import "./global.css"
 
 import Badge from './components/Badge'
+import BadgeNew from "./pages/BadgeNew";
 
 const name = 'Agneto';
 
@@ -22,9 +23,11 @@ const element2 = React.createElement('h1', {}, `Hola, soy ${name}`);
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<Badge
-    firstName="Des"
-    lastName="AN"
-    avatarUrl="https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon"
-    jobTitle="Frontend Engineer"
-    twitter="desagneto23"/>, container);
+// ReactDOM.render(<Badge
+//     firstName="Des"
+//     lastName="AN"
+//     avatarUrl="https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon"
+//     jobTitle="Frontend Engineer"
+//     twitter="desagneto23"/>, container);
+
+ReactDOM.render(<BadgeNew />, container);
---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import './styles/BadgeNew.css'
+
+import header from '../images/badge-header.svg'
+
+import Navbar from '../components/Navbar'
+import Badge from "../components/Badge";
+
+class BadgeNew extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+                <Navbar />
+                <div className="BadgeNew__hero">
+                    <img className="img-fluid" src={header} alt="Logo"/>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <Badge
+                                firstName="Des"
+                                lastName="AN"
+                                avatarUrl="https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon"
+                                jobTitle="Frontend Engineer"
+                                twitter="desagneto23"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BadgeNew;
