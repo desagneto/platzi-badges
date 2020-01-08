fb73404743fc175799d60941b2bfff4f5a58533d
Andrés Aguilar Nieto <desagneto@gmail.com>
2020-01-08 12:50:19 -0500

    clase mejorando user interface con layout 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import confLogo from "../images/astronauts.svg";
+
+import confLogo from "../images/platziconf-logo.svg";
+import astronauts from "../images/astronauts.svg";
+import {Link} from "react-router-dom";
 
 class Home extends React.Component{
 
@@ -9,9 +12,29 @@ class Home extends React.Component{
             <React.Fragment>
                 <div className="Badges">
                     <div className="Badges__hero">
-                        <div className="Badges__container">
-                            <img className="Badges_conf-logo" src={confLogo} alt="logo"/>
+
+                        <div className="container">
+
+                            <div className="row">
+
+                                <div className="col-md-4">
+
+                                    <img className="Badges_conf-logo" src={confLogo} alt="logo"/>
+
+                                    <Link to="/badges" className="btn btn-primary">Start</Link>
+
+                                </div>
+
+                                <div className="col-md-8">
+
+                                    <img className="Badges_conf-logo" src={astronauts} alt="logo"/>
+
+                                </div>
+
+                            </div>
+
                         </div>
+
                     </div>
                 </div>
             </React.Fragment>
