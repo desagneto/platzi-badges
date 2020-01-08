93eb3931a3dd878ab316ce3267cb28b7de34a887
Andrés Aguilar Nieto <desagneto@gmail.com>
2020-01-08 12:25:42 -0500

    clase mejorando user interface con layout 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 
+import Layout from './Layout';
 import Home from "../pages/Home"
 import Badges from "../pages/Badges";
 import BadgeNew from "../pages/BadgeNew";
-import Layout from './Layout';
 import NotFound from "../pages/NotFound";
 
 function App() {
     return(
         <BrowserRouter>
             <Layout>
-                <Switch/>
+                <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/badges" component={Badges} />
                     <Route exact path="/badges/new" component={BadgeNew} />
                     <Route component={NotFound} />
-                    {/*<Route exact path="/" component={NotFound}/>*/}
-                    {/*<Route render={() => <h1>Not found!</h1>} />*/}
-                    {/*<Route>*/}
-                    {/*    <NotFound />*/}
-                    {/*</Route>*/}
-                <Switch/>
+                </Switch>
             </Layout>
         </BrowserRouter>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import confLogo from "../images/astronauts.svg";
 
 class Home extends React.Component{
 
     render() {
 
         return (
-            <h1>Home</h1>
+            <React.Fragment>
+                <div className="Badges">
+                    <div className="Badges__hero">
+                        <div className="Badges__container">
+                            <img className="Badges_conf-logo" src={confLogo} alt="logo"/>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 
 }
 
-export  default Home;
+export default Home;
