d0c999fdb386dd03dcc5ab58813156e2bf940943
Andrés Aguilar Nieto <desagneto@gmail.com>
2020-01-10 09:29:26 -0500

    clase ui components y container components

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,92 +1,55 @@
 import React from 'react';
-
-import './styles/BadgeDetails.css'
-import confLogo from '../images/platziconf-logo.svg'
-import PageLoading from "../components/PageLoading";
-import PageError from "../components/PageError";
-import api from '../api'
+import confLogo from "../images/platziconf-logo.svg";
 import Badge from "../components/Badge";
 import {Link} from "react-router-dom";
 
-class BadgeDetails extends React.Component{
+function BadgeDetails(props) {
 
-    state = {
-      loading: true,
-      error: null,
-      data: undefined
-    };
+    const badge = props;
 
-    componentDidMount() {
-        this.fetchData();
-    }
-
-    fetchData = async () => {
-        this.setState({ loading: true, error: null });
-
-        try {
-            const data = await api.badges.read(
-                this.props.match.params.badgeId
-            );
-            this.setState({ loading: false, data: data });
-        } catch (error) {
-            this.setState({ loading: false, error: error });
-        }
-    };
-
-    render() {
-
-        if (this.state.loading) {
-            return <PageLoading/>
-        }
-
-        if(this.state.error){
-            return(
-                <PageError error={this.state.error} />
-            )
-        }
-
-        const badge = this.state.data;
-
-        return (
-            <div>
-                <div className="BadgeDetails__hero">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-6">
-                                <img src={confLogo} alt="logo"/>
-                            </div>
-                            <div className="col-6 BadgeDetails__hero-attendant-name">
-                                <h1>{badge.firstName} {badge.lastName}</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+    return(
+        <div>
+            <div className="BadgeDetails__hero">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-6">
-                            <Badge
-                                firstName={badge.firstName || 'FIRST_NAME'}
-                                lastName={badge.lastName || 'LAST_NAME' }
-                                jobTitle={badge.jobTitle || 'JOB_TITLE' }
-                                twitter={badge.twitter || 'twitter' }
-                                email={badge.email || 'EMAIL' }
-                            />
+                        <div className="col-6">
+                            <img src={confLogo} alt="logo"/>
                         </div>
-                        <div className="col-md-6">
-                            <div className="btn btn-primary">
-                                <Link className="text-reset" to={`/badges/${badge.id}/edit`}> Editar </Link>
+                        <div className="col-6 BadgeDetails__hero-attendant-name">
+                            <h1>{badge.firstName} {badge.lastName}</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <Badge
+                            firstName={badge.firstName || 'FIRST_NAME'}
+                            lastName={badge.lastName || 'LAST_NAME' }
+                            jobTitle={badge.jobTitle || 'JOB_TITLE' }
+                            twitter={badge.twitter || 'twitter' }
+                            email={badge.email || 'EMAIL' }
+                        />
+                    </div>
+                    <div className="col-md-6">
+                        <h2>Actions</h2>
+                        <div>
+                            <div>
+                                <Link className="btn btn-primary" to={`/badges/${badge.id}/edit`}> Editar </Link>
                             </div>
-                            <div className="btn btn-danger">
-                                <Link className="text-reset" to={`/badges/${badge.id}/edit`}> Eliminar </Link>
+                            <div>
+                                <button className="btn btn-danger"> Eliminar </button>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-        )
-    }
+
+        </div>
+    )
+
 }
 
 export default BadgeDetails;
